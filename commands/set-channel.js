4bdc1677aa6f2f8e2fe698c6fb199d12beecb87a
Ben Silverman <bensilverman10@gmail.com>
2020-08-09 15:44:46 +0100

    Write crude set-channel command

--- commands/set-channel.js
+++ commands/set-channel.js
@@ -6,6 +6,11 @@ module.exports = {
 	ownerOnly: true,
 	usage: "<channel ID>",
 	execute(message, args) {
+		const guildConf = message.client.settings.get(message.guild.id);
+		console.log(guildConf);
+		guildConf.countingChannelID = args[0];
+
+		console.log(guildConf);
 		message.channel.send("The counting channel has been updated!");
 	},
 };
