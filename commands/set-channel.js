b3ab7dd27aa5f5118cdb51259811593dbd9cb855
Ben Silverman <bensilverman10@gmail.com>
2020-08-26 17:47:30 +0100

    Update set-channel command to use enmap settings

--- commands/set-channel.js
+++ commands/set-channel.js
@@ -6,12 +6,15 @@ module.exports = {
 	ownerOnly: true,
 	usage: "<channel ID>",
 	execute(message, args) {
-		const guildConf = message.client.settings.get(message.guild.id);
 		const guildChannels = message.guild.channels.cache;
 
 		if (guildChannels.has(args[0])) {
-			guildConf.countingChannelID = args[0];
-			message.channel.send("The counting channel has been updated!");
+			message.client.settings.set("countingChannelID", args[0]);
+			message.channel.send(
+				`The counting channel has been updated and set to ${message.client.settings.get(
+					"countingChannelID",
+				)}`,
+			);
 		} else {
 			message.channel.send(
 				"I couldn't find that channel in this server. Make sure the ID is correct...",
