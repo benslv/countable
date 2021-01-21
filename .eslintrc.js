77f04ad3554d656878e691c3c426fda87d385280
Ben Silverman <bensilverman10@gmail.com>
2021-01-21 17:13:58 +0000

    Install eslint prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 12,
   },
