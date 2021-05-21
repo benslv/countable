5182f1f966532d33abac224e2ab471f7ae038f5c
Ben Silverman <bensilverman10@gmail.com>
2021-05-21 15:16:35 +0100

    Update set-nomessage command

--- src/commands/set-nomessage.js
+++ src/commands/set-nomessage.js
@@ -1,3 +1,5 @@
+const { embed } = require("../utils");
+
 module.exports = {
   name: "set-nomessage",
   description:
@@ -14,16 +16,27 @@ module.exports = {
         "noMessageReaction",
         JSON.parse(arg), // converts the string representation into a boolean value.
       );
-      message.channel.send(
-        `Messages now **${
-          arg === "true" ? "will" : "won't"
-        }** be reacted to when left empty.`,
-      );
+
+      message.channel.send({
+        embed: embed(message, {
+          type: "success",
+          title: "Setting updated!",
+          description: `Messages now **${
+            arg === "true" ? "will" : "won't"
+          }** be reacted to when left empty.`,
+        }),
+      });
+
       console.log(`Set "no message reaction" to ${arg}.`);
     } else {
-      message.channel.send(
-        "That's not a valid argument. Please enter `true` or `false`.",
-      );
+      message.channel.send({
+        embed: embed(message, {
+          type: "error",
+          title: "Invalid input.",
+          description:
+            "That's not a valid argument. Please enter `true` or `false`.",
+        }),
+      });
     }
   },
 };
