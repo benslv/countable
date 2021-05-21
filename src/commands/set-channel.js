67b8c91179eae97f2a4b4fe41f31bda170cca7a0
Ben Silverman <bensilverman10@gmail.com>
2021-05-21 15:05:07 +0100

    Update set-channel command

--- src/commands/set-channel.js
+++ src/commands/set-channel.js
@@ -1,3 +1,5 @@
+const { embed } = require("../utils");
+
 module.exports = {
   name: "set-channel",
   description:
@@ -10,27 +12,49 @@ module.exports = {
     const guildChannels = message.guild.channels.cache;
 
     // Match the ID of the channel, whether it's passed in as the raw ID or as a channel mention.
-    const [id] = args[0].match(/(\d{18})/g);
+    const match = args[0].match(/(\d{18})/g);
+
+    if (!match) {
+      return message.channel.send({
+        embed: embed(message, {
+          type: "error",
+          title: "Invalid input",
+          description:
+            "That doesn't look like the right format for a channel ID",
+        }),
+      });
+    }
+
+    const [id] = match;
 
     // Check that the channel ID exists in the guild.
     if (guildChannels.has(id)) {
       // If so, store the new ID in settings.
       gdb.set("channel", id);
 
-      message.client.channels
+      return message.client.channels
         .fetch(id)
-        .then(
-          channel =>
-            message.channel.send(
-              `The counting channel has been set to ${channel.toString()}`,
-            ),
-          console.log(`Counting channel ID set to ${id}.`),
-        )
+        .then(channel => {
+          message.channel.send({
+            embed: embed(message, {
+              type: "success",
+              title: "Channel updated!",
+              description: `The counting channel has been set to ${channel.toString()}`,
+            }),
+          });
+
+          console.log(`Counting channel ID set to ${id}.`);
+        })
         .catch(err => console.error(err));
-    } else {
-      message.channel.send(
-        "I couldn't find that channel in this server. Make sure the ID is correct...",
-      );
     }
+
+    return message.channel.send({
+      embed: embed(message, {
+        type: "error",
+        title: "Channel not found.",
+        description:
+          "I couldn't find that channel in this server. Make sure the ID is correct...",
+      }),
+    });
   },
 };
