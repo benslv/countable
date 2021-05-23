92c01954887fae907ccdc9e451880bb44983d40b
Ben Silverman <bensilverman10@gmail.com>
2021-05-23 17:15:19 +0100

    Initial version of leaderboard

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -1,3 +1,5 @@
+const { getUserScore, embed } = require("../utils");
+
 module.exports = {
   name: "leaderboard",
   description: "Displays the leaderboard for the current guild.",
@@ -5,9 +7,35 @@ module.exports = {
   guildOnly: true,
   ownerOnly: false,
   usage: "",
-  execute({ message, args, gdb }) {
+  execute: async ({ message, gdb }) => {
+    const medals = ["🥇", "🥈", "🥉"];
+
     // Get all users stored in gdb
-    // Calculate score (correct - incorrect) and accuracy for each
-    // Sort by score, then tiebreak on accuracy
+    const users = Array.from(Object.values(gdb.users));
+
+    // Sort by score
+    const top15 = users.sort(byScore).slice(0, 15);
+    console.log(top15);
+
+    const scoreStrings = [];
+
+    top15.forEach((user, i) => {
+      const score = getUserScore(user);
+
+      scoreStrings.push(
+        `${medals[i] ? medals[i] : "🔸"} **${score}** - <@${user.id}>`,
+      );
+    });
+
+    message.channel.send({
+      embed: embed(message, {
+        type: "info",
+        title: `Top counters in ${message.guild.name}`,
+        description: scoreStrings.join("\n"),
+        footer: "Sorted by score",
+      }),
+    });
   },
 };
+
+const byScore = (a, b) => getUserScore(b) - getUserScore(a);
