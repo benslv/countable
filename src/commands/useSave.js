c024ff0fcf9508568df40022a13158fba577f167
Ben Silverman <bensilverman10@gmail.com>
2021-06-06 12:00:51 +0100

    Add base command structure

---
+++ src/commands/useSave.js
@@ -0,0 +1,20 @@
+const { embed } = require("../utils");
+
+module.exports = {
+  name: "useSave",
+  args: false,
+  usage: "<",
+  guildOnly: false,
+  ownerOnly: false,
+  description: "Replies to the user to confirm the bot is running correctly!",
+};
+
+module.exports.execute = ({ message }) => {
+  message.channel.send({
+    embed: embed(message, {
+      type: "success",
+      title: "🏓 Pong!",
+      description: "Everything seems to be in order.",
+    }),
+  });
+};
