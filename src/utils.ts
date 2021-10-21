6f7a78366e5f0ca006306dfd57fe63af89e45631
Ben Silverman <bensilverman10@gmail.com>
2021-10-21 16:17:15 +0100

    Clean up utils folder

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,3 @@
-import { timeStamp } from "console";
-import {
-  EmbedField,
-  Message,
-  MessageEmbed,
-  MessageEmbedAuthor,
-} from "discord.js";
 import { userT } from "./@types/guild";
 
 /**
@@ -12,75 +5,6 @@ import { userT } from "./@types/guild";
  */
 export function isNumber(s: string): boolean {
   return /^\d+$/.test(s);
-}
-
-type embed_kind = "INFO" | "ERROR" | "SUCCESS";
-
-// /**
-//  * Renders a discord embed
-//  */
-// export function embed(
-//   message: Message,
-//   kind: embed_kind,
-//   content: Partial<MessageEmbed>,
-//   verbose = false,
-// ): MessageEmbed {
-//   const colour = (() => {
-//     switch (kind) {
-//       case "INFO": return 0xffa630;
-//       case "ERROR": return 0xe84855;
-//       case "SUCCESS": return 0x4aeb47;
-//     }
-//   })();
-
-//   let embed = new MessageEmbed();
-
-//   embed.color = colour;
-//   embed = { ...embed, ...content } as MessageEmbed;
-
-//   if (verbose) {
-//     embed.footer = {
-//       text: `Requested by ${message.author.tag}`,
-//       iconURL: message.author.avatarURL(),
-//     }
-//     embed.timestamp = new Date().getTime() / 1000; // TODO is this meant to be unix?
-//   }
-
-//   return embed;
-// };
-
-export function embed(
-  message,
-  { type = "info", title = "", description = "", ...rest },
-  verbose = false,
-) {
-  const types = {
-    info: 0xffa630,
-    error: 0xe84855,
-    success: 0x4aeb47,
-  };
-
-  let output = {
-    color: types[type],
-    title: title,
-    description: description,
-    ...rest,
-  };
-
-  if (verbose) {
-    output = {
-      ...output,
-      // TODO bad. very very bad.
-      // @ts-ignore
-      footer: {
-        text: `Requested by ${message.author.tag}`,
-        icon_url: message.author.avatarURL(),
-      },
-      timestamp: new Date(),
-    };
-  }
-
-  return output;
 }
 
 export function getUserScore(user: userT): number {
