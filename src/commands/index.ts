b3d7bcfb2835a6bb1da627ce2c8b837728d1014b
Ben Silverman <bensilverman10@gmail.com>
2021-10-21 16:17:26 +0100

    Add leaderboard command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,8 +2,9 @@ import { Collection } from "discord.js";
 
 import * as ping from "./ping";
 import * as server from "./server";
+import * as leaderboard from "./leaderboard";
 
-export const registry = [ping, server];
+export const registry = [ping, server, leaderboard];
 
 export const commands = new Collection(
   registry.map(command => [command.metadata.name, command]),
---
+++ src/commands/leaderboard.ts
@@ -0,0 +1,65 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js";
+
+import { getUserScore } from "../utils";
+
+import { userT } from "../@types/guild";
+
+const info = {
+  name: "leaderboard",
+  aliases: ["board", "top", "scoreboard"],
+  description: "Displays the leaderboard for the current guild.",
+  checkArgs: (): boolean => true,
+  guildOnly: true,
+  ownerOnly: false,
+  usage: "",
+};
+
+export const metadata = new SlashCommandBuilder()
+  .setName(info.name)
+  .setDescription(info.description);
+
+function byScore(a, b): number {
+  return getUserScore(b) - getUserScore(a);
+}
+
+export async function execute(interaction, gdb) {
+  const medals = ["🥇", "🥈", "🥉"];
+
+  // Get all users stored in gdb
+  const users: userT[] = Array.from(Object.values(gdb.users));
+
+  // Sort by score
+  const top15 = users.sort(byScore).slice(0, 15);
+
+  const scoreStrings = [];
+
+  let i = 0;
+  for (const user of top15) {
+    let userInfo;
+    try {
+      userInfo = await interaction.client.users.fetch(user.id);
+    } catch (err) {
+      console.error(`Unable to find user info for ${user.id}`);
+    }
+    const score = getUserScore(user);
+
+    scoreStrings.push(
+      `${medals[i] || "🔸"} **${score}** - ${
+        userInfo ? userInfo.tag : "*Unknown User*"
+      }`,
+    );
+
+    i += 1;
+  }
+
+  return interaction.reply({
+    embeds: [
+      new MessageEmbed()
+        .setTitle(`Top counters in ${interaction.guild.name}`)
+        .setDescription(scoreStrings.join("\n"))
+        .setFooter("Sorted by player score")
+        .setTimestamp(new Date()),
+    ],
+  });
+}
