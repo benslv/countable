11ba987bfc4c453092f7f5bc3a9375c2311cb695
Ben Silverman <bensilverman10@gmail.com>
2021-10-22 17:18:41 +0100

    Add stats command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,8 +3,9 @@ import { Collection } from "discord.js";
 import * as ping from "./ping";
 import * as server from "./server";
 import * as leaderboard from "./leaderboard";
+import * as stats from "./stats";
 
-export const registry = [ping, server, leaderboard];
+export const registry = [ping, server, leaderboard, stats];
 
 export const commands = new Collection(
   registry.map(command => [command.metadata.name, command]),
---
+++ src/commands/stats.ts
@@ -0,0 +1,92 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js";
+
+const info = {
+  name: "stats",
+  aliases: [],
+  description: "Replies to the user to confirm the bot is running correctly!",
+  checkArgs: args => args.length == 0 || args.length == 1,
+  guildOnly: true,
+  ownerOnly: false,
+  usage: "<blank> or <user mention>",
+};
+
+export const metadata = new SlashCommandBuilder()
+  .setName(info.name)
+  .setDescription(info.description)
+  .addUserOption(option =>
+    option
+      .setName("user")
+      .setDescription("The user you want information about!"),
+  );
+
+export async function execute(interaction, gdb) {
+  const user = interaction.options.getUser("user");
+
+  if (user) {
+    const response = await getUserStats(interaction, gdb, user.id);
+
+    return interaction.reply({ embeds: [response] });
+  }
+
+  const response = await getUserStats(interaction, gdb, interaction.member.id);
+
+  return interaction.reply({ embeds: [response] });
+}
+
+async function getUserStats(interaction, gdb, id) {
+  if (!gdb.users[id]) {
+    return new MessageEmbed()
+      .setTitle("User not found")
+      .setDescription(
+        "Sorry, I couldn't find that user. Have they done any counting yet?",
+      );
+  }
+
+  const correct = gdb.users[id].correct;
+  const incorrect = gdb.users[id].incorrect;
+  const score = (correct - incorrect).toString();
+  const accuracy = `${(100 * (correct / (correct + incorrect))).toFixed(2)}%`;
+  const points = gdb.users[id].points;
+
+  const user = await interaction.client.users.fetch(id);
+
+  return new MessageEmbed()
+    .setTitle(`Stats for ${user.tag}`)
+    .setDescription(`Here are the stats for ${user.toString()}!`)
+    .setThumbnail(
+      user.avatarURL({ size: 128, dynamic: true }) || user.defaultAvatarURL,
+    )
+    .addFields(
+      {
+        name: "Total Counts",
+        value: correct + incorrect,
+        inline: true,
+      },
+      {
+        name: "Correct",
+        value: correct,
+        inline: true,
+      },
+      {
+        name: "Incorrect",
+        value: incorrect,
+        inline: true,
+      },
+      {
+        name: "Score",
+        value: score,
+        inline: true,
+      },
+      {
+        name: "Accuracy",
+        value: accuracy,
+        inline: true,
+      },
+      {
+        name: "Money",
+        value: points,
+        inline: true,
+      },
+    );
+}
