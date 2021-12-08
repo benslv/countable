21ee5c0aa08cb759a7f01edf00048ac3606675c1
Ben Silverman <bensilverman10@gmail.com>
2021-12-08 00:02:43 +0000

    Write addSave command

---
+++ src/commands/save.ts
@@ -0,0 +1,95 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { guildDB } from "../@types/guild";
+import { embedError, embedSuccess } from "../utils";
+
+const info = {
+  name: "save",
+  description: "Add or list saves in your server.",
+};
+
+export const metadata = new SlashCommandBuilder()
+  .setName(info.name)
+  .setDescription(info.description)
+  .addSubcommand(subcommand =>
+    subcommand
+      .setName("list")
+      .setDescription("List the current save points in the server."),
+  )
+  .addSubcommand(subcommand =>
+    subcommand
+      .setName("add")
+      .setDescription("Add a save to the server.")
+      .addIntegerOption(option =>
+        option.setName("count").setDescription("The count to add the save at."),
+      ),
+  );
+
+export async function execute(interaction, gdb: guildDB) {
+  const subcommand = interaction.options.getSubcommand();
+
+  switch (subcommand) {
+    case "list":
+      return listSaves(interaction, gdb);
+    case "add":
+      return addSave(interaction, gdb);
+  }
+}
+
+function listSaves(interaction, gdb) {
+  const saves = gdb.get("saves");
+
+  console.log("saves");
+}
+
+function addSave(interaction, gdb: guildDB) {
+  const count = interaction.options.getInteger("count");
+
+  if (count < 0) {
+    return interaction.reply({
+      embeds: [
+        embedError
+          .setTitle("Invalid count.")
+          .setDescription("Counts must be a positive integer."),
+      ],
+    });
+  }
+
+  const user = gdb.getUser(interaction.member);
+
+  if (user.points < gdb.savePrice) {
+    return interaction.reply({
+      embeds: [
+        embedError
+          .setTitle("Not enough points.")
+          .setDescription(
+            "You haven't got enough points to buy a save.\nDo some counting to collect more!",
+          ),
+      ],
+    });
+  }
+
+  if (count > gdb.nextCount) {
+    return interaction.reply({
+      embeds: [
+        embedError
+          .setTitle("Save point too high.")
+          .setDescription(
+            "You can't add a save point higher than the current count!",
+          ),
+      ],
+    });
+  }
+
+  gdb.addSave(count);
+  gdb.set(`users.${interaction.member.id}.points`, user.points - gdb.savePrice);
+
+  return interaction.reply({
+    embeds: [
+      embedSuccess
+        .setTitle("Save point added!")
+        .setDescription(
+          `A save point has been added at the count \`${count}\`.`,
+        ),
+    ],
+  });
+}
