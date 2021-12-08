c3481b309114a03d0901a91a16d78c697e4c9c27
Ben Silverman <bensilverman10@gmail.com>
2021-12-08 18:04:22 +0000

    Add set command to registry

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,8 +5,9 @@ import * as server from "./server";
 import * as leaderboard from "./leaderboard";
 import * as stats from "./stats";
 import * as milestone from "./milestone";
+import * as set from "./set";
 
-export const registry = [ping, server, leaderboard, stats, milestone];
+export const registry = [ping, server, leaderboard, stats, milestone, set];
 
 export const commands = new Collection(
   registry.map(command => [command.metadata.name, command]),
