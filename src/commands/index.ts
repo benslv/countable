34c2d585832147d6949361e5b47119ce1b884f9d
Ben Silverman <bensilverman10@gmail.com>
2021-12-09 22:57:58 +0000

    Add remaining commands to registry

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -6,8 +6,17 @@ import * as leaderboard from "./leaderboard";
 import * as stats from "./stats";
 import * as milestone from "./milestone";
 import * as set from "./set";
+import * as save from "./save";
 
-export const registry = [ping, server, leaderboard, stats, milestone, set];
+export const registry = [
+  ping,
+  server,
+  leaderboard,
+  stats,
+  milestone,
+  set,
+  save,
+];
 
 export const commands = new Collection(
   registry.map(command => [command.metadata.name, command]),
